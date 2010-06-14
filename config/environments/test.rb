3f551cea3694addd2fe8611a111fafa4c59db207
hava sayed <hava@bright-sun.org>
2010-06-14 15:47:14 +0100

    User model with passwords, and user show page

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -30,3 +30,5 @@ config.action_mailer.delivery_method = :test
 config.gem 'rspec-rails', :version => '>= 1.3.2', :lib => false unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec-rails'))
 
 config.gem "webrat", :version => ">=0.4.3"
+
+config.gem 'factory_girl'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,8 +15,10 @@ require 'spec_helper'
 describe User do
   before(:each) do
     @valid_attributes = {
-      :name => "hava",
-      :email => "hava@gmail.com"
+            :name => "hava",
+            :email => "hava@gmail.com",
+            :password => "foobar",
+            :password_confirmation => "foobar"
     }
   end
 
@@ -41,19 +43,19 @@ describe User do
   end
 
   it "should reject invalid email addresses" do
-    addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
+    addresses = %w[     user@foo,com user_at_foo.org example.user@foo.     ]
     addresses.each do |address|
       invalid_email_user = User.new(@valid_attributes.merge(:email => address))
       invalid_email_user.should_not be_valid
     end
   end
 
-   it "should reject duplicate email addresses" do
+  it "should reject duplicate email addresses" do
     # Put a user with given email address into the database.
     User.create!(@valid_attributes)
     user_with_duplicate_email = User.new(@valid_attributes)
     user_with_duplicate_email.should_not be_valid
-   end
+  end
 
   it "should reject email addresses identical up to case" do
     upcased_email = @valid_attributes[:email].upcase
@@ -61,4 +63,79 @@ describe User do
     user_with_duplicate_email = User.new(@valid_attributes)
     user_with_duplicate_email.should_not be_valid
   end
+
+  describe "password validations" do
+
+    it "should require a password" do
+      User.new(@valid_attributes.merge(:password => "", :password_confirmation => "")).
+              should_not be_valid
+    end
+
+    it "should require a matching password confirmation" do
+      User.new(@valid_attributes.merge(:password_confirmation => "invalid")).
+              should_not be_valid
+    end
+
+    it "should reject short passwords" do
+      short = "a" * 5
+      hash = @valid_attributes.merge(:password => short, :password_confirmation => short)
+      User.new(hash).should_not be_valid
+    end
+
+    it "should reject long passwords" do
+      long = "a" * 41
+      hash = @valid_attributes.merge(:password => long, :password_confirmation => long)
+      User.new(hash).should_not be_valid
+    end
+
+  end
+
+  describe "password encryption" do
+
+    before(:each) do
+      @user = User.create!(@valid_attributes)
+    end
+
+    it "should have an encrypted password attribute" do
+      @user.should respond_to(:encrypted_password)
+    end
+
+    it "should set the encrypted password" do
+      @user.encrypted_password.should_not be_blank
+    end
+
+    describe "authenticate method" do
+
+      it "should return nil on email/password mismatch" do
+        wrong_password_user = User.authenticate(@valid_attributes[:email], "wrongpass")
+        wrong_password_user.should be_nil
+      end
+
+      it "should return nil for an email address with no user" do
+        nonexistent_user = User.authenticate("bar@foo.com", @valid_attributes[:password])
+        nonexistent_user.should be_nil
+      end
+
+      it "should return the user on email/password match" do
+        matching_user = User.authenticate(@valid_attributes[:email], @valid_attributes[:password])
+        matching_user.should == @user
+      end
+    end
+  end
+
+  describe "has_password? method" do
+    before(:each) do
+      @user = User.create!(@valid_attributes)
+    end
+
+    it "should be true if the passwords match" do
+      @user.has_password?(@valid_attributes[:password]).should be_true
+    end
+
+    it "should be false if the passwords don't match" do
+      @user.has_password?("invalid").should be_false
+    end
+  end
+
+
 end
